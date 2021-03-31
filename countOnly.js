be2013e72499de5350820c27652f7218cb90a80f
Austin <sicardaustin97@gmail.com>
2021-03-31 18:47:16 +0000

    Function that takes an array and object, checks how many of the same variables are in our array and keeps a count of the running number of them. The way it keeps track of them is by creating a new object to store the running count in.

---
+++ countOnly.js
@@ -0,0 +1,51 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+const countOnly = function (allItems, itemsToCount) {
+  const finalResults = {};
+
+  for (let all in allItems) {
+
+    if (itemsToCount[all]) {
+
+      if (finalResults[all]) {
+
+        finalResults[all] += 1;
+
+      } else {
+
+        finalResults[all] = 1;
+
+      }
+    }
+  }
+
+  return finalResults;
+
+};
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
