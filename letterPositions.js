72778607db30d492247c9410647ecf21395fb04f
Austin <sicardaustin97@gmail.com>
2021-03-31 22:53:27 +0000

    Updated comments for much better understanding

--- letterPositions.js
+++ letterPositions.js
@@ -48,25 +48,26 @@ function assertArraysEqual(actual, expected) {
   };
 
 
-const letterPositions = function(wordTest) {
+const letterPositions = function(wordTest) {    /* Declaring letterPositions as a function with the 
+                                                   parameters "wordtest" which is expected to be a String */
 
-  const results = {};
-  var i = -1;
+  const results = {};    // Object "reselts" is being declared empty so we can fill it with the correct information
+  var i = -1;           // Var "i" is a number that is keeping track of which index the letter is at
 
-  for (let word of wordTest) {
+  for (let word of wordTest) {    // This for..of statement goes through each letter individually
 
-    i++;
+    i++;    // Each time a new letter is looped we add one to "i" because it's keeping track of the index (of the letter)
 
-    if (word !== " ") {
+    if (word !== " ") {   // Skipping spaces (Notice how we keep this inside so our index number "i" stays accurate)
       
-      if (results[wordTest[i]]) {
+      if (results[wordTest[i]]) {   
         
         results[wordTest[i]].push(i);
       
       } else {
 
-        results[wordTest[i]] = [];
-        results[wordTest[i]].push(i);
+        results[wordTest[i]] = [];         // Creates an empty array inside the object results so "i" can be pushed
+        results[wordTest[i]].push(i);     // Push the letter 
 
       }
     }
