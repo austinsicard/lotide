50cc2001c7ce77561a8cfa3aaad3895bee358129
Austin <sicardaustin97@gmail.com>
2021-04-05 18:38:49 +0000

    Adjustments to head function

--- head.js
+++ head.js
@@ -1,23 +1,14 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} ===  ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 head = function (first) {
 
-  console.log(first[0]);
+  let firstElem = first[0];
+  return firstElem;
   
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
