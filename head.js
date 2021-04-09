ed12987bfac5a559501173ab3da46f6c49591531
Austin <sicardaustin97@gmail.com>
2021-04-09 02:56:53 +0000

    Removed spaces

--- head.js
+++ head.js
@@ -1,14 +1,9 @@
 const assertEqual = require('./assertEqual');
 
-head = function (first) {
-
+const head = function (first) {
   let firstElem = first[0];
   return firstElem;
-  
 };
-
-// TEST CODE
-
-
 //assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head(['b', 1, 'f', 6]));
 module.exports = head;
