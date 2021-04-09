ce2afb2587cd399b5bc50418823105ab7fb608bd
Austin <sicardaustin97@gmail.com>
2021-04-09 20:39:31 +0000

    Updated with mocha&chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -8,6 +8,9 @@ describe("#middle", () => {
   it("returns 2, 3 for [1, 2, 3, 4]", () => {
     assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]); 
   });
+  it("returns [] for []", () => {
+    assert.deepEqual(middle([]), []); 
+  });
 });
 
 // assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,17 @@
 const assert = require('chai').assert;
 const tail = require('../tail');
 
-
 describe("#tail", () => {
-  it("returns 'Labs' for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
-    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ['Lighthouse', 'Labs']);
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []); 
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []); 
   });
 });
+
 // const words = ["Yo Yo", "Lighthouse", "Labs"];
 // assertEqual(words.length, 3);
