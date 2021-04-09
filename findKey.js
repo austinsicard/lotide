043322c6001df4bfde2751f6ec3d715994ac5fa1
Austin <sicardaustin97@gmail.com>
2021-04-09 02:36:52 +0000

    Required assertEqual, implemented assertEqual in test cases

--- findKey.js
+++ findKey.js
@@ -11,15 +11,12 @@ const findKey = function(obj, callback) {
 return undefined;
 };
 
+const ourObject = {
+  "Canada": { population: 38.44},
+  "MERICA": { population: 331.42 },
+  "Mexico": { population: 129.97 },
+  "India":  { population: 1390.38 },
+  "Japan":  { population: 125.28 }
+}
 
-
-
-
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), "noma") // => "noma"
+assertEqual(findKey(ourObject, x => x.population === 125.28), "Japan") // => "mexico"
