4d93b33e591bc5a8e6c4757ca6d5ef683926899a
Austin <sicardaustin97@gmail.com>
2021-04-09 19:53:20 +0000

    Remove map, updated headtest with Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,28 @@
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
+
+// const head = require('../head');
+// const assertEqual = require('../assertEqual');
+
+
+    // Passes
+
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+// console.log("------------L-i-n-e--B-r-e-a-k------------");
+
+//     // Fails
+// assertEqual(head([3, 5, 9]), 8);
+// assertEqual(head([]), 7);
+// assertEqual(head(["This", "Is", "Wrong"]), "Hello");
+
