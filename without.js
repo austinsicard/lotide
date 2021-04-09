3ce198b19f63ecab87afc5edab273a066f772808
Austin <sicardaustin97@gmail.com>
2021-04-09 19:46:28 +0000

    Small update to test if git is removing files

--- without.js
+++ without.js
@@ -1,19 +1,23 @@
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+
 const without = function(source, itemsToRemove) {
 
   const results = [];
-  for (let j = 0 ; j < itemsToRemove.length ; j++) {
-  for (let i = 0 ; i < source.length ; i++) {
-    if (source[i] !== itemsToRemove[j]) {
-      results.push(source[i]);
-    }
+  if (eqArrays(source, itemsToRemove)) {
+  
+    return results;
   }
+    for (let j = 0 ; j < itemsToRemove.length ; j++) {
+      for (let i = 0 ; i < source.length ; i++) {
+        if (source[i] !== itemsToRemove[j]) {
+          results.push(source[i]);
+        }
+      }
 }
-  console.log(results);
+  return results;
 };
 
-
-
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]);
-// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-  // Passed asserArrayEqual testing
+//Passes
+assertArraysEqual(without(["Austin", "Sicard", "Learns"], ["Sicard"]), ["Austin", "Learns"]);
+assertArraysEqual(without(["Austin", "Sicard", "Learns"], ["Austin", "Sicard", "Learns"]), []);
