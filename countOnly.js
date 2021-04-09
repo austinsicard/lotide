8aa0bdc8d8102c1a31146d049d2ff5c22eee9ed2
Austin <sicardaustin97@gmail.com>
2021-04-09 01:56:36 +0000

    Removed assertEqual and replaced it with assertObjectsEqual, I did this because you can't compare objects with assertEqual

--- countOnly.js
+++ countOnly.js
@@ -1,18 +1,9 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} ===  ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-
+const assertObjectsEqual = require('./assertObjectsEqual');
 
 const countOnly = function (allItems, itemsToCount) {
   const results = {};
 
-  for (let item in allItems) {
+  for (let item of allItems) {
 
     if (itemsToCount[item]) {
 
@@ -42,12 +33,9 @@ const firstNames = [
   "Fang",
   "Joe"
 ];
-
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
-
-console.log(result1[Jason]);
+assertObjectsEqual(result1["Jason"], 1);
+// assertObjectsEqual(result1["Karima"], undefined);
+assertObjectsEqual(result1["Fang"], 2);
+// assertObjectsEqual(result1["Agouhanna"], undefined);
